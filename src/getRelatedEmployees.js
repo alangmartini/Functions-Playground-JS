9c4c1e67e3e9010bfc87b7ed8172e143cc246900
gmaralan <gmartinialan@gmail.com>
2022-09-29 14:17:55 -0400

    finished req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aquidasdsads
+  return !data.employees
+    .find((employee) => employee.id === id)
+    .id.length === 0;
 }
 
+// Filters for employees in which managers array includes
+// managerId parameter, returning an array of objects.
+// Map the array so the each object become an string of firstName + lastName
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  return data.employees.filter((employee) =>
+    employee.managers.includes(managerId))
+    .map((func) =>
+      `${func.firstName} ${func.lastName}`);
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
