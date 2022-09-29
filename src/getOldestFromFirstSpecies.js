a1af9b5e5a0369e66def3dda8534f2ebb17a3732
gmaralan <gmartinialan@gmail.com>
2022-09-29 18:49:54 -0400

    finished req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((employeeU) => employeeU.id === id);
+  const firstResponsibleId = employee.responsibleFor[0];
+  const firstResponsableSpecies = data.species.find((specie) =>
+    specie.id === firstResponsibleId);
+  const animal = firstResponsableSpecies.residents.reduce((acc, curr) => {
+    if (curr.age > acc.age) return curr;
+    return acc;
+  });
+  const { name, sex, age } = animal;
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies(data.employees[0].id));
 
 module.exports = getOldestFromFirstSpecies;
