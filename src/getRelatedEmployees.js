8f84873a9ac67554521c116c7d60e9fe65843549
gmaralan <gmartinialan@gmail.com>
2022-09-29 14:29:52 -0400

    finished req 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,21 +1,23 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return !data.employees
-    .find((employee) => employee.id === id)
-    .id.length === 0;
+  return data.employees
+    .some((employee) => employee.managers.includes(id));
 }
 
 // Filters for employees in which managers array includes
 // managerId parameter, returning an array of objects.
 // Map the array so the each object become an string of firstName + lastName
 function getRelatedEmployees(managerId) {
-  return data.employees.filter((employee) =>
-    employee.managers.includes(managerId))
-    .map((func) =>
-      `${func.firstName} ${func.lastName}`);
-}
+  const managersEmployees = data.employees.filter((employee) =>
+    employee.managers.includes(managerId));
+  console.log(isManager(managerId));
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+  return managersEmployees.map((func) =>
+    `${func.firstName} ${func.lastName}`);
+}
 
 module.exports = { isManager, getRelatedEmployees };
