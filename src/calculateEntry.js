4b9c9e92d92c9c986061b17232e507c73f6e9af1
gmaralan <gmartinialan@gmail.com>
2022-09-29 16:34:05 -0400

    finished req 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,39 @@
 const data = require('../data/zoo_data');
 
+function getRange(age) {
+  let ageRange = '';
+  switch (true) {
+  case age < 18:
+    ageRange = 'child';
+    break;
+  case age >= 18 && age < 50:
+    ageRange = 'adult';
+    break;
+  default:
+    ageRange = 'senior';
+  }
+  return ageRange;
+}
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const entriesNum = {};
+  entrants.forEach((person) => {
+    const { age } = person;
+    const ageRange = getRange(age);
+    if (Object.keys(entriesNum).includes(ageRange)) {
+      entriesNum[ageRange] += 1;
+    } else {
+      entriesNum[ageRange] = 1;
+    }
+  });
+  return entriesNum;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const entrances = countEntrants((entrants));
+  return Object.keys(entrances).reduce((acc, curr) =>
+    acc + ((entrances[curr] * data.prices[curr])), 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,6 +5,4 @@ function getSpeciesByIds(...args) {
   return args.map((item) => data.species.find((specie) => specie.id === item));
 }
 
-const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
-getSpeciesByIds([lionId]);
 module.exports = getSpeciesByIds;
