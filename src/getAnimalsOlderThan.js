74e5df91d853f73bba9293c1b0aa5859c9f79799
gmaralan <gmartinialan@gmail.com>
2022-09-29 13:59:17 -0400

    finished req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specieInfo = data.species.find((specie) => specie.name === animal);
+  return specieInfo.residents.every((residente) => residente.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
