94449510bd58bbb12a4b459dd668e8faea86e522
gmaralan <gmartinialan@gmail.com>
2022-09-29 13:53:50 -0400

    finished req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...args) {
+  if (args.length < 1) return [];
+  return args.map((item) => data.species.find((specie) => specie.id === item));
 }
 
+const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
+getSpeciesByIds([lionId]);
 module.exports = getSpeciesByIds;
